b0bc52535d8003846dafa7f61b9039ded9d45cae
anthony Moon <anthonymoon@live.com.au>
2019-04-28 18:31:08 +0000

    final adjustments and addition of comments

--- main.c
+++ main.c
@@ -4,98 +4,97 @@
 int main(void)
 {
     char message[1000], c[1000], ct[1000], m;  //maximum number of code to be entered
-	int i;
-	int j;
-	int index;
+	int i; //a variable for alphabets
+	int j; //a variable for alphabets
+	int index; //to store converted alphabets
 	int shift;   //allows input value for number of shift of letters up to 26
 	int option;
     printf("1 is decryption with rotation cipher \n2 is encryption with rotation cipher \n3 is decryption with subsitution cipher \n4 is encrpytion with substitution cipher \nChoose from case 1-4: \n");
-    fflush(stdout);
+    fflush(stdout); //allows code to buffer
     scanf(" %d", &option);
-    //getchar();    //allows use of gets rather than just scanf
-        for (i=0; i<26; i++) {
+        for (i=0; i<26; i++) { //counts i in increments until 26 which are the alphabets
             ct[i]=0;
         }
         
-    if(option==1)
+    if(option==1) //decryption using rotation cipher
     {
     	printf("\nEnter a message to decrypt: \n");
-    	fflush(stdout);
-    	scanf(" %[^\n]s",message);  //gets allows readings of single strings on keyboard
+    	fflush(stdout); //allows code to buffer
+    	scanf(" %[^\n]s",message);  //%[^\n]s allows multiple independent format specifiers
     	printf("Enter number of shift: \n");  //selection of number of rotation
-    	fflush(stdout);
-    	scanf("%d", &shift);
+    	fflush(stdout); //allows code to buffer
+    	scanf("%d", &shift); //input of integer
     	
-    	for(i=0; message[i] !='\0'; ++i)
+    	for(i=0; message[i] !='\0'; ++i) 
     	{
-            if (message[i]<='z' && message[i]>='a') // check if lowercase
+            if (message[i]<='z' && message[i]>='a') // check if code contains lowercases
             {  
-                message[i]=message[i]-'a'+'A'; // convert to uppercase
+                message[i]=message[i]-'a'+'A'; // converts lowercases to uppercases
             }
             
     		m=message[i];
     		
-    		if(m>='A' && m<='Z')
+    		if(m>='A' && m<='Z') //checks for uppercases
     		{
-    			m=m-shift;
+    			m=m-shift; //allows shift of alphabets
     			if(m<'A')
     			{
-    				m=m+'Z'-'A'+ 1;
+    				m=m+'Z'-'A'+ 1; //keeps the uppercases
     			}
     			message[i]=m;
     		}
     	}
     	
-    	printf("\nDecrypted message: %s \n", message);
-    	printf("\n\n\n");
-    } else if (option==2)
+    	printf("\nCiphered message is: %s \n", message);
+    	printf("\n\n\n"); //allows three blank paragraph at the end for neatness
+    } else if (option==2) //encryption using rotation cipher
     {
     	printf("\nEnter a message to encrypt: \n");
-    	fflush(stdout);
-    	scanf(" %[^\n]s ",message);
+    	fflush(stdout); //allows code to buffer
+    	scanf(" %[^\n]s ",message); //%[^\n]s allows multiple independent format specifiers
     	printf("\nEnter number of shift: \n");
-    	fflush(stdout);
+    	fflush(stdout); //allows code to buffer
     	scanf("%d", &shift);
     	
     	for(i=0; message[i] !='\0'; ++i)
     	{
-            if (message[i]<='z' && message[i]>='a') // check if lowercase
+            if (message[i]<='z' && message[i]>='a') // check if code contains lowercases
             { 
-                message[i]=message[i]-'a'+'A'; // convert to uppercase
+                message[i]=message[i]-'a'+'A'; // converts lowercases to uppercases
             }
 
     		m=message[i];
     		
-    		if(m>='A' && m<='Z')
+    		if(m>='A' && m<='Z') //checks for uppercases
     		{
-    			m=m+shift;
+    			m=m+shift; //allows shift of alphabets
     			if(m>'Z')
     			{
-    				m=m-'Z'+'A'- 1;
+    				m=m-'Z'+'A'- 1; //keeps the uppercases
     			}
     			message[i]=m;
     		}
 
     	}
     		
-    	printf("\nEncrpyted message: %s \n", message);
-    	printf("\n\n\n");
-    } else if(option==3)
+    	printf("\nCiphered message is: %s \n", message);
+    	printf("\n\n\n"); //allows three blank paragraph at the end for neatness
+    } else if(option==3) //decryption using substitution cipher
     {
         
         printf(" \nEnter decrypted message: \n");
-        fflush(stdout);
-        scanf(" %[^\n]s",message);
+        fflush(stdout); //allows code to buffer
+        scanf(" %[^\n]s",message); //%[^\n]s allows multiple independent format specifiers
         
         printf("\nEnter Key From A to Z: \n");
         printf("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
-        fflush(stdout);
-        scanf(" %[^\n]s", c);
+        fflush(stdout); //allows code to buffer
+        scanf(" %[^\n]s", c); //%[^\n]s allows multiple independent format specifiers
 
-        for(i = 0; i < 26; i++)
+        for(i = 0; i < 26; i++) //counts i in increments up to 26 which are the alphabets
         {
-            if (c[i]<='z' && c[i]>='a') {  // check if lowercase
-                c[i]=c[i]-'a'+'A'; // convert to uppercase
+            if (c[i]<='z' && c[i]>='a') {  // check if code contains lowercases
+                c[i]=c[i]-'a'+'A'; // convert lowercases to uppercases
             }
             
             
@@ -103,11 +102,11 @@ int main(void)
       
         for(i = 0; i < strlen(message); i++)
         {
-            if (message[i]<='z' && message[i]>='a') {  // check if lowercase
-                message[i]=message[i]-'a'+'A'; // convert to uppercase
+            if (message[i]<='z' && message[i]>='a') { //checks if code contains lowercases
+                message[i]=message[i]-'a'+'A'; //converts lowercases into uppercases
             }
           
-            index = message[i]- 65;
+            index = message[i]- 65; //uses ASCII table to convert lowercases into uppercases
             
             if(0<=index && index<26){
                 ct[i] = c[index];   // stores corresponding encypted letter in ct if a capital letter
@@ -116,36 +115,36 @@ int main(void)
             }
         }
       
-        printf("\nCipher message is: %c", ct[i]);
+        printf("\nCiphered message is: %c", ct[i]);
         for(i = 0; i < strlen(message); i++)
         {
             printf("%c", ct[i]);
         }
-        printf("\n\n\n");
-    } else if(option==4)
+        printf("\n\n\n"); //allows three blank paragraph at the end for neatness
+    } else if(option==4) //encryption using substitution cipher
     {
         
-        printf(" \nEnter decrypted message: \n");
-        fflush(stdout);
-        scanf(" %[^\n]s",message);
+        printf(" \nEnter encrypted message: \n");
+        fflush(stdout); //allows code to buffer
+        scanf(" %[^\n]s",message); //%[^\n]s allows multiple independent format specifiers
         
         printf("\nEnter Key From A to Z: \n");
         printf("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
-        fflush(stdout);
-        scanf(" %[^\n]s", c);
+        fflush(stdout); //allows code to buffer
+        scanf(" %[^\n]s", c); //%[^\n]s allows multiple independent format specifiers
 
         for(i = 0; i < 26; i++)
         {
-            if (c[i]<='z' && c[i]>='a') {  // check if lowercase
-                c[i]=c[i]-'a'+'A'; // convert to uppercase
+            if (c[i]<='z' && c[i]>='a') {  // check if code contains lowercases
+                c[i]=c[i]-'a'+'A'; // convert lowercases to uppercases
             }
             
         }
         
         for (i=0; i<strlen(message); i++) 
         {
-            if (message[i]<='z' && message[i]>='a') {  // check if lowercase
-                message[i]=message[i]-'a'+'A';  // convert to uppercase
+            if (message[i]<='z' && message[i]>='a') {  // check if code contains  lowercases
+                message[i]=message[i]-'a'+'A';  //converts lowercases into uppercases
             }
             for (j=0; j<26; j++) 
             {
@@ -156,12 +155,12 @@ int main(void)
             }
         }
         
-        printf("\nCipher message is: %c", ct[i]);
+        printf("\nCiphered message is: %c", ct[i]);
         for(i = 0; i < strlen(message); i++)
         {
             printf("%c", ct[i]);
         }
-        printf("\n\n\n");
+        printf("\n\n\n"); //allows three blank paragraph at the end for neatness
     }
     	return 0;
 }
